f4ac9e6dad0b1f5bc449770b6133911b7fdaaee4
ahmed.elgammal <a.t.elgammal@gmail.com>
2024-04-24 01:16:22 +0200

    make the node output info with loop of 1 second sleep

--- src/my_pkg/src/simple_welcome_node.cpp
+++ src/my_pkg/src/simple_welcome_node.cpp
@@ -1,12 +1,19 @@
-#include <rclcpp/rclcpp.hpp>
+#include "rclcpp/rclcpp.hpp"
 
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<rclcpp::Node>("welcome_node");
-    RCLCPP_INFO(node->get_logger(), "Hello cpp Node");
+    rclcpp::Node::SharedPtr node = std::make_shared<rclcpp::Node>("welcome_node");
+    int counter = 1;
+
+    while (rclcpp::ok())
+    {
+        RCLCPP_INFO(node->get_logger(), "Hello world");
+        rclcpp::sleep_for(std::chrono::nanoseconds(1000000000));
+        counter++;
+    }
+    
     rclcpp::spin(node);
     rclcpp::shutdown();
-    
     return 0;
 }
